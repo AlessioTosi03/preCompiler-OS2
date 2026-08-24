be24be9153e0c4a720fead5216f1d445339659ae
verof <verofantasy@gmail.com>
2026-08-24 11:34:04 +0200

    migliorata gestione errori I/O e corretto parsing degli identificatori
    - Aggiunta gestione dettagliata degli errori I/O
    - Inserito controllo per file di input vuoti e gestione errori in fclose
    - Corretta la logica del ciclo di scansione identificatori in mark_used
    
    spostate definizioni strutture dati in precompiler.h
    
    - Spostati typedef struct (Var, Err, Vars, Errs, Strs, Buf) da precompiler.c a precompiler.h
    - Aggiunta documentazione nei commenti per rispettare i requisiti della traccia
    
    - separato il parsing degli argomenti brevi e lunghi in funzioni helper dedicate
    - eliminato l'annidamento dei cicli e la modifica diretta degli indici nel loop principale
    - risolti i warning di cognitive complexity e deep nesting

--- main.c
+++ main.c
@@ -7,45 +7,77 @@ static void usage(void) {
     fprintf(stderr, "Uso: myPreCompiler -i <file_input.c> [-o <file_output>] [-v]\n");
 }
 
+static int parse_short_value(int argc, char **argv, int *i, size_t j, const char **target) {
+    const char *value = argv[*i][j+1] ? argv[*i] + j+1 : NULL;
+    if (!value) {
+        if (*i +1 < argc) {
+            *i +=1;
+            value = argv[*i];
+        } else {
+            return 0;
+        }
+    }
+    *target = value;
+    return 1;
+}
+
+static int parse_short_opts(int argc, char **argv, int *i, const char **in, const char **out, int *verbose) {
+    for (size_t j = 1; argv[*i][j] != '\0'; ++j) {
+        char option = argv[*i][j];
+        if (option == 'v') {
+            *verbose =1;
+        } else if (option == 'i') {
+            return parse_short_value(argc, argv, i, j, in);
+        } else if (option == 'o') {
+            return parse_short_value(argc, argv, i, j, out);
+        } else {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+static int parse_long_opts(int argc, char **argv, int *i, const char **in, const char **out, int *verbose) {
+    const char *arg =argv[*i];
+
+    if (!strncmp(arg, "--in=", 5)) {
+        *in = arg + 5;
+    } else if (!strncmp(arg, "--out=", 6)) {
+        *out = arg + 6;
+    } else if (!strncmp(arg, "--verbose", 7)) {
+        *verbose = 1;
+    } else if (!strncmp(arg, "--in", 4)) {
+        if (*i +1 < argc) {
+            *i +=1;
+            *in = argv[*i];
+        } else {
+            return 0;
+        }
+    } else if (!strncmp(arg, "--out", 5)) {
+        if (*i +1 < argc) {
+            *i +=1;
+            *out = argv[*i];
+        } else {
+            return 0;
+        }
+    } else {
+        return 0;
+    }
+    return 1;
+}
+
 int main(int argc, char **argv) {
     const char *in = NULL;
     const char *out = NULL;
     int verbose = 0;
 
     for (int i = 1; i < argc; ++i) {
-        if (!strncmp(argv[i], "--in=", 5)) {
-            in = argv[i] + 5;
-        } else if (!strncmp(argv[i], "--out=", 6)) {
-            out = argv[i] + 6;
-        } else if (!strcmp(argv[i], "--verbose")) {
-            verbose = 1;
-        } else if (!strncmp(argv[i], "--in", 4)) {
-            in = ++i < argc ? argv[i] : NULL;
-        } else if (!strncmp(argv[i], "--out", 5)) {
-            out = ++i < argc ? argv[i] : NULL;
-        } else if (argv[i][0] == '-' && argv[i][1] != '-' && argv[i][1] != '\0') {
-            for (size_t j = 1; argv[i][j] != '\0'; ++j) {
-                char option = argv[i][j];
-                if (argv[i][j] == 'v') {
-                    verbose = 1;
-                } else if (option == 'i' || option == 'o') {
-                    const char *value = argv[i][j + 1] ? argv[i] + j + 1 : (++i < argc ? argv[i] : NULL);
-                    if (!value) {
-                        usage();
-                        return 1;
-                    }
-                    if (option == 'i') {
-                        in = value;
-                    } else {
-                        out = value;
-                    }
-                    break;
-                } else {
-                    usage();
-                    return 1;
-                }
+        if (argv[i][0] == '-' && argv[i][1] != '-' && argv[i][1] != '\0') {
+            if (!parse_short_opts(argc, argv, &i, &in, &out, &verbose)) {
+                usage();
+                return 1;
             }
-        } else {
+        } else if (!parse_long_opts(argc, argv, &i, &in, &out, &verbose)) {
             usage();
             return 1;
         }
